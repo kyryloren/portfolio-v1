34b592e621b9fcc99959040d00ccf12e4f8bd864
kyryloren <dev@kyryloorlov.com>
2020-03-08 20:18:50 -0400

    yay i updated it [-_-]

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -4,7 +4,4 @@ module.exports = [{
     },{
       plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
       options: {"plugins":[],"trackingId":"UA-159840334-1","head":true},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
     }]
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -7,7 +7,9 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Avatar from '../common/avatar';
 
-const HeroWrapper = styled.div`height: 100vh;`;
+const HeroWrapper = styled.div`
+  height: 100vh;
+`;
 
 const StyledLink = styled(ExternalLink)`
   position: relative;
@@ -38,61 +40,79 @@ const StyledLink = styled(ExternalLink)`
 `;
 
 const StyledButton = styled.a`
-	padding: 15px 30px;
-	font-size: 20px;
-	font-weight: 900;
-	color: #fff;
-	background-color: #00b388;
-	border: 2px solid #00b388;
-	cursor: pointer;
-	width: max-content;
-	transition: 0.25s ease;
-	text-decoration: none;
+  padding: 15px 30px;
+  font-size: 20px;
+  font-weight: 900;
+  color: #fff;
+  background-color: #00b388;
+  border: 2px solid #00b388;
+  cursor: pointer;
+  width: max-content;
+  transition: 0.25s ease;
+  text-decoration: none;
 
-	&:hover {
-		box-shadow: 0 6px 18px 0 rgba(#000, 0.1);
-		transform: translateY(-6px);
-	}
+  &:hover {
+    box-shadow: 0 6px 18px 0 rgba(#000, 0.1);
+    transform: translateY(-6px);
+  }
 `;
 
 function Hero() {
-	return (
-		<HeroWrapper>
-			<Container fluid>
-				<Row>
-					<Col className="d-flex align-items-center min-vh-100">
-						<Container fluid>
-							<Row className="justify-content-md-center">
-								<Col lg={5} className="text-center">
-									<Row className="justify-content-center">
-										<Avatar />
-									</Row>
-									<Row>
-										<h2>
-											<span role="img" aria-label="wave">
-												&#128075;
-											</span>{' '}
-											Wasssup I'm <StyledLink href="https://instagram.com/kyryloren">Kyrylo</StyledLink>
-											, a freelance developer and UX/UI designer based in New York. Why don't you go get a cup of coffee
-											while I make your stuff.
-										</h2>
-									</Row>
-									<Row className="justify-content-center mt-4">
-										<StyledButton href="https://github.com/kyryloren" rel="noreferrer noopener" target="_blank">
-											View GitHub
-										</StyledButton>
-										<StyledButton href={`resume.pdf`} rel="noreferrer noopener" target="_blank" className="ml-4">
-											View Resume
-										</StyledButton>
-									</Row>
-								</Col>
-							</Row>
-						</Container>
-					</Col>
-				</Row>
-			</Container>
-		</HeroWrapper>
-	);
+  return (
+    <HeroWrapper>
+      <Container fluid>
+        <Row>
+          <Col className="d-flex align-items-center min-vh-100">
+            <Container fluid>
+              <Row className="justify-content-md-center">
+                <Col lg={6} className="text-center">
+                  <Row className="justify-content-center">
+                    <Avatar />
+                  </Row>
+                  <Row>
+                    <h2>
+                      <span role="img" aria-label="wave">
+                        &#128075;
+                      </span>{' '}
+                      Hey I'm{' '}
+                      <StyledLink href="https://instagram.com/kyryloren">
+                        Kyrylo
+                      </StyledLink>
+                      , a freelance developer and UX/UI designer based in New
+                      York. Have a project in mind or just wanna see what I can
+                      do? I think you should{' '}
+                      <StyledLink href="mailto:dev@kyryloorlov.com">
+                        contact me
+                      </StyledLink>
+                      .
+                    </h2>
+                  </Row>
+                  <Row className="justify-content-center mt-4">
+                    <StyledButton
+                      className="m-3"
+                      href="https://github.com/kyryloren"
+                      rel="noreferrer noopener"
+                      target="_blank"
+                    >
+                      View GitHub
+                    </StyledButton>
+                    <StyledButton
+                      className="m-3"
+                      href={`resume.pdf`}
+                      rel="noreferrer noopener"
+                      target="_blank"
+                    >
+                      View Resume
+                    </StyledButton>
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </HeroWrapper>
+  );
 }
 
 export default Hero;
